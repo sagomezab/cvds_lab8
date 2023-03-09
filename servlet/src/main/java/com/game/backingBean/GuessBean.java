c9f048c767caf4fae0cafd22649505891e5cc514
David Valencia <cardoda11@gmail.com>
2023-03-09 15:57:12 -0500

    Fin Partes

---
+++ servlet/src/main/java/com/game/backingBean/GuessBean.java
@@ -0,0 +1,73 @@
+
+package com.game.backingBean;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import java.util.Random;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "guessBean")
+@SessionScoped
+public class GuessBean implements Serializable{
+    private int premio;
+    private String estado;
+    private Random random;
+    private int numeroAdivinar;
+    private int numeroIntentos; 
+    private int intento;
+
+
+    public GuessBean(){
+        random = new Random();
+        numeroAdivinar = random.nextInt(100);
+        premio = 100000;
+        estado = "Sin Ganador";
+        numeroIntentos = 0;
+    }
+    public int getPremio() {
+        return premio;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public int getNumeroAdivinar() {
+        return numeroAdivinar;
+    }
+    public int getNumeroIntentos(){
+        return this.numeroIntentos;
+    }
+    public int getIntento(){
+        return intento;
+    }
+    public void setPremio(int premio) {
+        this.premio = premio;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public void setNumeroAdivinar(int numeroAdivinar) {
+        this.numeroAdivinar = numeroAdivinar;
+    }
+    public void setIntento(int a){
+        this.intento = a;
+    }
+    public void guess(){
+        if(estado.equals("Sin Ganador")){
+            if(numeroAdivinar == intento){
+                estado = "Gano";
+                premio = 100000;
+            }
+            else{
+                premio -= 10000;
+                estado = "Sin Ganador";
+            }
+            numeroIntentos++;
+        }
+   }
+    public void restart(){
+        numeroAdivinar = random.nextInt(100);
+        premio = 100000;
+        numeroIntentos = 0;
+    }
+    
+}
